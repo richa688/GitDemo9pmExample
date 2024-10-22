79626426f6c6c74235c10a82564af8ea62689cd0
richa688 <task.stadsolution@gmail.com>
2024-10-22 21:50:40 +0530

    third change

--- GitTestDemo/src/demo/HelloProgram.java
+++ GitTestDemo/src/demo/HelloProgram.java
@@ -5,6 +5,7 @@ public class HelloProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("New program");
+		System.out.println("this is git hub change");
 
 	}
 
