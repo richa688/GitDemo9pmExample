379e991565ff129627e57ef7f99e00ece6a3ceb9
Admin <Admin@DESKTOP-PD5DKQ2>
2024-10-22 21:39:38 +0530

    first commit

---
+++ GitTestDemo/src/demo/TestJava.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class TestJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello this is git example");
+	}
+
+}
