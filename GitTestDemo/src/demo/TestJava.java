8057437cebe223f317f3c6d946e036cb5ad62f7f
Admin <Admin@DESKTOP-PD5DKQ2>
2024-10-28 21:12:16 +0530

    first commit

--- GitTestDemo/src/demo/TestJava.java
+++ GitTestDemo/src/demo/TestJava.java
@@ -5,6 +5,7 @@ public class TestJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello this is git example");
+System.out.println("It is todays update 28-10-2024 :21:11");
 	}
 
 }
